e1fe29b844f93ee512c53c29bfd4be3b2b271742
azitra <adriel.artiza@gmail.com>
2019-07-12 11:02:29 +0800

    remove model

--- mongo/mongodb.go
+++ mongo/mongodb.go
@@ -1,10 +1,7 @@
 package mongo
 
 import (
-	"crypto/rand"
 	"errors"
-	"go-serverless-example/model"
-	"math/big"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -15,8 +12,6 @@ type DB struct {
 	conn *mgo.Session
 	user *mgo.Collection
 }
-
-var maxRand = big.NewInt(1<<63 - 1)
 
 // NewMongoDB initialize mongodb with credential and source
 func NewMongoDB(host, username, password, database, source string) (*mgo.Session, error) {
@@ -39,31 +34,24 @@ func (db *DB) Close() {
 	db.conn.Close()
 }
 
-func randomID() (int64, error) {
-	// Get a random number within the range [0, 1<<63-1)
-	n, err := rand.Int(rand.Reader, maxRand)
-	if err != nil {
-		return 0, err
-	}
-	// Don't assign 0.
-	return n.Int64() + 1, nil
-}
-
-// DeleteUser -
+// DeleteUser delete user in database
 func (db *DB) DeleteUser(id string) error {
 	return db.user.Remove(bson.D{{Name: "id", Value: id}})
 }
 
-// GetUserByID -
-func (db *DB) GetUserByID(id string) (*model.User, error) {
-	user := &model.User{}
+// GetUserByID get user using by id
+// @param id search key
+// @param user object to cast
+func (db *DB) GetUserByID(id string, user interface{}) error {
 	if err := db.user.Find(bson.D{{Name: "id", Value: id}}).One(user); err != nil {
-		return user, errors.New(err.Error())
+		return errors.New(err.Error())
 	}
-	return user, nil
+	return nil
 }
 
-// GetUser -
+// GetUser get user using by email
+// @param email search key
+// @param user object to cast
 func (db *DB) GetUser(email string, user interface{}) error {
 	if err := db.user.Find(bson.D{{Name: "email", Value: email}}).One(user); err != nil {
 		return err
