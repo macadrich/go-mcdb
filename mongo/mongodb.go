5e4900fa68b0c2168b17554e55915464cf30a11d
azitra <adriel.artiza@gmail.com>
2019-07-12 16:33:21 +0800

    add check table name

--- mongo/mongodb.go
+++ mongo/mongodb.go
@@ -7,12 +7,24 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// DB -
+const (
+	// MongoDBErrTableName table name not set return error
+	MongoDBErrTableName = "mongodb: table not set"
+)
+
+// TableName holds table name type string
+type TableName string
+
+func (tn TableName) length() int {
+	return len(tn)
+}
+
+// DB hold fields data that need in connection for database
 type DB struct {
 	Conn         *mgo.Session
 	DatabaseName string
-	TableName    string
-	Collection   map[string]*mgo.Collection
+	tablename    TableName
+	Collection   map[TableName]*mgo.Collection
 }
 
 // NewMongoDB initialize mongodb with credential and source
@@ -31,7 +43,7 @@ func NewMongoDB(host, username, password, database, source string) (*DB, error) 
 	return &DB{
 		Conn:         conn,
 		DatabaseName: database,
-		Collection:   make(map[string]*mgo.Collection),
+		Collection:   make(map[TableName]*mgo.Collection),
 	}, nil
 }
 
@@ -42,25 +54,29 @@ func (db *DB) Close() {
 
 // AddTable add table in mongo database
 func (db *DB) AddTable(tableName string) {
-	db.Collection[tableName] = db.Conn.DB(db.DatabaseName).C(tableName)
+	tb := TableName(tableName)
+	db.Collection[tb] = db.Conn.DB(db.DatabaseName).C(string(tb))
 }
 
 // Table query specific table
 func (db *DB) Table(name string) *DB {
-	db.TableName = name
+	db.tablename = TableName(name)
 	return db
 }
 
 // Delete delete item in database
 func (db *DB) Delete(key string, value string) error {
-	return db.Collection[db.TableName].Remove(bson.D{{Name: key, Value: key}})
+	return db.Collection[db.tablename].Remove(bson.D{{Name: key, Value: key}})
 }
 
 // Get get item using by id
 // @param key search key
 // @param item object to cast
 func (db *DB) Get(key string, value string, item interface{}) error {
-	if err := db.Collection[db.TableName].Find(bson.D{{Name: key, Value: value}}).One(item); err != nil {
+	if db.tablename.length() <= 0 {
+		return errors.New(MongoDBErrTableName)
+	}
+	if err := db.Collection[db.tablename].Find(bson.D{{Name: key, Value: value}}).One(item); err != nil {
 		return errors.New(err.Error())
 	}
 	return nil
@@ -68,7 +84,10 @@ func (db *DB) Get(key string, value string, item interface{}) error {
 
 // Add saves a given item, assigning it a new ID.
 func (db *DB) Add(item interface{}) (id string, err error) {
-	if err := db.Collection[db.TableName].Insert(item); err != nil {
+	if db.tablename.length() <= 0 {
+		return "", errors.New(MongoDBErrTableName)
+	}
+	if err := db.Collection[db.tablename].Insert(item); err != nil {
 		return "", errors.New(err.Error())
 	}
 	return id, nil
@@ -76,12 +95,18 @@ func (db *DB) Add(item interface{}) (id string, err error) {
 
 // Update update fields in database
 func (db *DB) Update(key string, value string, item interface{}) error {
-	return db.Collection[db.TableName].Update(bson.D{{Name: key, Value: value}}, item)
+	if db.tablename.length() <= 0 {
+		return errors.New(MongoDBErrTableName)
+	}
+	return db.Collection[db.tablename].Update(bson.D{{Name: key, Value: value}}, item)
 }
 
 // List view list of data
 func (db *DB) List(items interface{}) error {
-	if err := db.Collection[db.TableName].Find(nil).All(items); err != nil {
+	if db.tablename.length() <= 0 {
+		return errors.New(MongoDBErrTableName)
+	}
+	if err := db.Collection[db.tablename].Find(nil).All(items); err != nil {
 		return err
 	}
 	return nil
