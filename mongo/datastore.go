478c766a3aa99ba31b31aad236d8733ab5136354
azitra <adriel.artiza@gmail.com>
2019-07-12 13:58:36 +0800

    update table query

--- mongo/datastore.go
+++ mongo/datastore.go
@@ -3,6 +3,7 @@ package mongo
 // Datastore mongo database interface
 type Datastore interface {
 	AddTable(tableName string)
+	Table(name string) Datastore
 	Delete(key string) error
 	Get(key string, item interface{}) error
 	Add(item interface{}) (id string, err error)
